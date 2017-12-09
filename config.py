9efe905eb1949d0671cb9c92691261c84b39764f
Leonard Sheiba <lsheiba@gmail.com>
2017-12-09 01:18:14 -0800

    Update config.py

--- config.py
+++ config.py
@@ -29,11 +29,11 @@ flags.DEFINE_float('regularization_scale', 0.392, 'regularization coefficient fo
 flags.DEFINE_string('dataset', 'mnist', 'The name of dataset [mnist, fashion-mnist')
 flags.DEFINE_boolean('is_training', True, 'train or predict phase')
 flags.DEFINE_integer('num_threads', 8, 'number of threads of enqueueing exampls')
-flags.DEFINE_string('logdir', os.environ['TARGET_DIR', 'logs directory')
+flags.DEFINE_string('logdir', os.environ['TARGET_DIR'], 'logs directory')
 flags.DEFINE_integer('train_sum_freq', 100, 'the frequency of saving train summary(step)')
 flags.DEFINE_integer('val_sum_freq', 500, 'the frequency of saving valuation summary(step)')
 flags.DEFINE_integer('save_freq', 3, 'the frequency of saving model(epoch)')
-flags.DEFINE_string('results', 'results', 'path for saving results')
+flags.DEFINE_string('results', os.environ['TARGET_DIR'], 'path for saving results')
 
 ############################
 #   distributed setting    #
